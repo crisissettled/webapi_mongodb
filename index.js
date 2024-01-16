9a5fa6d86132431540cdaf631a6f4a8a1b7f4ba5
James Liu <jliu@sirokygroup.com>
2024-01-16 11:41:56 -0500

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
+
+const app = express();
+
+app.use(express.json()).use("/api", routes);
+
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`);
+});
+
+app.use("/api", routes);
